47e557c999283a1151af5fdb23ecd904e5e19687
Carlos Castillo <sir.ragnar@gmail.com>
2015-03-15 10:10:30 +0100

    app refactored as enclosure

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,40 +1,41 @@
 'use strict';
-
-/**
- * @ngdoc overview
- * @name todoApp
- * @description
- * # todoApp
- *
- * Main module of the application.
- */
-angular
-  .module('todoApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.sortable',
-    'LocalStorageModule'
-  ])
-  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
-    localStorageServiceProvider.setPrefix('ls');
-  }])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'mainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+(function (angular, undefined) {
+  /**
+   * @ngdoc overview
+   * @name todoApp
+   * @description
+   * # todoApp
+   *
+   * Main module of the application.
+   */
+  angular
+    .module('todoApp', [
+      'ngAnimate',
+      'ngCookies',
+      'ngMessages',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch',
+      'ui.sortable',
+      'LocalStorageModule'
+    ])
+    .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+      localStorageServiceProvider.setPrefix('ls');
+    }])
+    .config(function ($routeProvider) {
+      $routeProvider
+        .when('/', {
+          templateUrl: 'views/main.html',
+          controller: 'MainCtrl',
+          controllerAs: 'mainCtrl'
+        })
+        .when('/about', {
+          templateUrl: 'views/about.html',
+          controller: 'AboutCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    });
+}(window.angular));
